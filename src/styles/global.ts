98bcfb044d8a12dfada8a850ba34ec15a4461f26
Caio Lucas <caiulucas.ps@gmail.com>
2022-06-27 15:56:52 -0300

    style: add responsive font-size

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,16 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    @media screen and (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media screen and (max-width: 768px) {
+      font-size: 87.5%;
+    }
+  }
+
   body {
     background-color: ${({ theme }) => theme.colors.GRAY_900};
     font-family: ${({ theme }) => theme.fonts.SECONDARY};
