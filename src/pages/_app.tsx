9710c83eecbbb11d6c78b6525989eb80346010e0
Caio Lucas <caiulucas.ps@gmail.com>
2022-06-27 15:54:58 -0300

    feat: Header added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from '../styles/global';
+import { theme } from '../themes/theme';
+
+import { Header } from './AppStyle';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <Header>
+        <div>
+          <h1>
+            <span>@</span> AfroLetter
+          </h1>
+        </div>
+      </Header>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
