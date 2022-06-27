25ed9a31dfafd6e2eda7f31c7d8a4ce3147ee4d3
Caio Lucas <caiulucas.ps@gmail.com>
2022-06-27 16:40:12 -0300

    feat: add news list to index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,65 @@
 import type { NextPage } from 'next';
+import { UserOutlined, CalendarOutlined } from '@ant-design/icons';
+
+import { Container, NewsCard } from '../styles/IndexStyle';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <p>Hello, World!</p>
-    </div>
+    <Container>
+      <ul>
+        <NewsCard>
+          <h2>Learning Next.js with Typescript and eslint</h2>
+          <p>
+            This is a simple description example for a newsletter prototype
+            project in figma. The only functionality of this text is pass a
+            simple view of how things are going to be in the application.
+          </p>
+
+          <div>
+            <p>
+              <UserOutlined /> Caio Lucas
+            </p>
+            <time>
+              <CalendarOutlined /> 01 de agosto de 2022
+            </time>
+          </div>
+        </NewsCard>
+        <NewsCard>
+          <h2>Learning Next.js with Typescript and eslint</h2>
+          <p>
+            This is a simple description example for a newsletter prototype
+            project in figma. The only functionality of this text is pass a
+            simple view of how things are going to be in the application.
+          </p>
+
+          <div>
+            <p>
+              <UserOutlined /> Caio Lucas
+            </p>
+            <time>
+              <CalendarOutlined /> 01 de agosto de 2022
+            </time>
+          </div>
+        </NewsCard>
+        <NewsCard>
+          <h2>Learning Next.js with Typescript and eslint</h2>
+          <p>
+            This is a simple description example for a newsletter prototype
+            project in figma. The only functionality of this text is pass a
+            simple view of how things are going to be in the application.
+          </p>
+
+          <div>
+            <p>
+              <UserOutlined /> Caio Lucas
+            </p>
+            <time>
+              <CalendarOutlined /> 01 de agosto de 2022
+            </time>
+          </div>
+        </NewsCard>
+      </ul>
+    </Container>
   );
 };
 
