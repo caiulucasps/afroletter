6460bab9c164de031e58250aac4325f5610f86eb
Caio Lucas <caiulucas.ps@gmail.com>
2022-06-29 17:32:18 -0300

    feat: add prismic provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,40 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import { PrismicProvider } from '@prismicio/react';
+import { PrismicPreview } from '@prismicio/next';
+
+import { linkResolver, repositoryName } from '../../prismicio';
 
 import GlobalStyles from '../styles/global';
 import { theme } from '../themes/theme';
 
 import { Header } from '../styles/AppStyle';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <Header>
-        <div>
-          <h1>
-            <span>@</span> AfroLetter
-          </h1>
-        </div>
-      </Header>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Header>
+          <div>
+            <h1>
+              <span>@</span> AfroLetter
+            </h1>
+          </div>
+        </Header>
+        <PrismicPreview repositoryName={repositoryName}>
+          <Component {...pageProps} />
+        </PrismicPreview>
+      </ThemeProvider>
+    </PrismicProvider>
   );
 }
 
